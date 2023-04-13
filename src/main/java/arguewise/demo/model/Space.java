994c6d1956c75e594f0e0e829468ae6f3b91f489
venetrius <gergelygjuhasz@gmail.com>
2023-04-13 15:40:03 +0200

    add Space model

---
+++ src/main/java/arguewise/demo/model/Space.java
@@ -0,0 +1,36 @@
+package arguewise.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "spaces")
+public class Space {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
+    private String name;
+
+    @NotBlank(message = "Description cannot be blank")
+    @Size(min = 10, max = 200, message = "Description must be between 10 and 200 characters")
+    private String description;
+
+}
+
