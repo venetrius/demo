587a8b1b1c6c1a2ce7a6d653e453fb29568053fa
venetrius <gergelygjuhasz@gmail.com>
2023-04-21 14:34:38 +0200

    FE add register component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Layout } from 'antd';
 import HeaderComponent from './components/Header/Header.jsx';
 import FooterComponent from './components/Footer/Footer.js';
 import Login from './components/Auth/Login';
-import { AuthProvider, useAuth } from './contexts/AuthContext';
+import Register from './components/Auth/Register';
+
+import { AuthProvider } from './contexts/AuthContext';
 
 const { Content } = Layout;
 
 const App = () => {
   return (
     <AuthProvider>
-      <Layout className="layout">
-        <HeaderComponent />
-        <Content style={{ padding: '0 50px', marginTop: 64 }}>
-          <Login />
-        </Content>
-        <FooterComponent />
-      </Layout>
+      <Router>
+        <Layout className="layout">
+          <HeaderComponent />
+          <Content style={{ padding: '0 50px', marginTop: 64 }}>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </Content>
+          <FooterComponent />
+        </Layout>
+      </Router>
     </AuthProvider>
-  );
+    );
 };
 
 export default App;
--- frontend/src/components/Auth/Register.js
+++ frontend/src/components/Auth/Register.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Form, Input, Button, Checkbox, Card } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import { useAuth } from '../../contexts/AuthContext.js';
+import { Form, Input, Button, Card } from 'antd';
+import { UserOutlined, LockOutlined, MailOutlined } from '@ant-design/icons';
+import { useAuth } from '../../contexts/AuthContext';
 import { Link } from 'react-router-dom';
 
-const Login = () => {
+const Register = () => {
   const { login } = useAuth();
 
   const onFinish = async (values) => {
-    console.log('Authenticating user: ', values.email);
-    const response = await fetch('http://localhost:8080/api/auth/authenticate', {
+    const response = await fetch('http://localhost:8080/api/auth/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -21,17 +20,15 @@ const Login = () => {
       const data = await response.json();
       login(data.token);
     } else {
-      // TODO Handle error
     }
   };
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-
-      <Card title="Login" style={{ width: 300 }}>
+      <Card title="Register" style={{ width: 300 }}>
         <Form
-          name="normal_login"
-          className="login-form"
+          name="normal_register"
+          className="register-form"
           initialValues={{
             remember: true,
           }}
@@ -39,6 +36,21 @@ const Login = () => {
         >
           <Form.Item
             name="email"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Email!',
+              },
+              {
+                type: 'email',
+                message: 'Please enter a valid email!',
+              },
+            ]}
+          >
+            <Input prefix={<MailOutlined className="site-form-item-icon" />} placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            name="userName"
             rules={[
               {
                 required: true,
@@ -63,21 +75,12 @@ const Login = () => {
               placeholder="Password"
             />
           </Form.Item>
-          <Form.Item>
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-
-            <a className="login-form-forgot" href="#">
-              Forgot password
-            </a>
-          </Form.Item>
 
           <Form.Item>
-            <Button type="primary" htmlType="submit" className="login-form-button" style={{ width: '100%' }}>
-              Log in
+            <Button type="primary" htmlType="submit" className="register-form-button" style={{ width: '100%' }}>
+              Register
             </Button>
-            Or <Link to="/register">register now!</Link>
+            Or <Link to="/">log in</Link>
           </Form.Item>
         </Form>
       </Card>
@@ -85,4 +88,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
