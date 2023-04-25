acee6a824b4a1d89d1341a77bf41422b42513fe7
venetrius <gergelygjuhasz@gmail.com>
2023-04-25 08:49:27 +0200

    FE update header to use react-router-dom

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
+import { NavLink } from 'react-router-dom';
 
 const { Header } = Layout;
 
 const HeaderComponent = () => {
   return (
-    <Header>
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-        <Menu.Item key="1">
-          <a href="/">Home</a>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <a href="/spaces">Spaces</a>
-        </Menu.Item>
-        <Menu.Item key="3">
-          <a href="/profile">Profile</a>
-        </Menu.Item>
-      </Menu>
-    </Header>
+<Header>
+  <div className="logo" />
+  <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+    <Menu.Item key="1" >
+      <NavLink to="/" >
+        Home
+      </NavLink>
+    </Menu.Item>
+    <Menu.Item key="2">
+      <NavLink to="/spaces" >
+        Spaces
+      </NavLink>
+    </Menu.Item>
+    <Menu.Item key="3">
+      <NavLink to="/profile" >
+        Profile
+      </NavLink>
+    </Menu.Item>
+  </Menu>
+</Header>
   );
 };
 
