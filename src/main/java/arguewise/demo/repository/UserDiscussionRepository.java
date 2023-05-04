c243ee9cb8dc2ef58b554de55dd761540a035fec
venetrius <gergelygjuhasz@gmail.com>
2023-05-04 14:47:35 +0200

    BE add user discussion repository

---
+++ src/main/java/arguewise/demo/repository/UserDiscussionRepository.java
@@ -0,0 +1,22 @@
+package arguewise.demo.repository;
+
+import arguewise.demo.model.Discussion;
+import arguewise.demo.model.User;
+import arguewise.demo.model.UsersDiscussion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserDiscussionRepository extends JpaRepository<UsersDiscussion, Long> {
+    List<UsersDiscussion> findByUserId(Long userId);
+
+    List<UsersDiscussion> findByDiscussionId(Long discussionId);
+
+   Optional<UsersDiscussion> findByUserIdAndDiscussionId(int id, Long id1);
+
+    Optional<UsersDiscussion> findByUserAndDiscussion(User user, Discussion discussion);
+}
+
