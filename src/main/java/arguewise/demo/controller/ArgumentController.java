3d998962f9a8829a9b3c4e73ac717ea691ab486e
venetrius <gergelygjuhasz@gmail.com>
2023-05-05 15:36:26 +0200

    BE add Controllers for Argument resource

---
+++ src/main/java/arguewise/demo/controller/ArgumentController.java
@@ -0,0 +1,62 @@
+package arguewise.demo.controller;
+
+import arguewise.demo.dto.argument.ArgumentResponseDTO;
+import arguewise.demo.dto.argument.UpdateArgumentDTO;
+import arguewise.demo.model.Argument;
+import arguewise.demo.service.IArgumentService;
+import arguewise.demo.service.IDiscussionService;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/arguments") // TODO improve mapping, root should not be api in this class
+
+public class ArgumentController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ArgumentController.class);
+
+    @Autowired
+    private IArgumentService argumentService;
+
+    @Autowired
+    private IDiscussionService discussionService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ArgumentResponseDTO> getArgumentById(@PathVariable Long id) {
+        logger.info("Received request to get argument by id: {}", id);
+        Optional<Argument> argument = argumentService.findById(id);
+        if (argument.isPresent()) {
+            return ResponseEntity.ok(new ArgumentResponseDTO(argument.get()));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ArgumentResponseDTO> updateArgument(@PathVariable Long id, @Valid @RequestBody UpdateArgumentDTO updatedArgument) {
+        logger.info("Received request to update argument with id: {}", id);
+        if (argumentService.findById(id).isPresent()) {
+            argumentService.update(id, updatedArgument);
+            return ResponseEntity.ok(new ArgumentResponseDTO(argumentService.update(id, updatedArgument)));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteArgument(@PathVariable Long id) {
+        logger.info("Received request to delete argument with id: {}", id);
+        if (argumentService.findById(id).isPresent()) {
+            argumentService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ src/main/java/arguewise/demo/controller/DiscussionArgumentController.java
@@ -0,0 +1,58 @@
+package arguewise.demo.controller;
+
+import arguewise.demo.dto.argument.ArgumentResponseDTO;
+import arguewise.demo.dto.argument.CreateArgumentDTO;
+import arguewise.demo.model.Argument;
+import arguewise.demo.service.IArgumentService;
+import arguewise.demo.service.IDiscussionService;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("api/discussions/{discussionId}/arguments")
+public class DiscussionArgumentController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ArgumentController.class);
+
+    @Autowired
+    private IArgumentService argumentService;
+
+    @Autowired
+    private IDiscussionService discussionService;
+
+    @GetMapping
+    public ResponseEntity<List<ArgumentResponseDTO>> getAllArguments(@PathVariable Long discussionId) {
+        logger.info("Received request to get all arguments for discussion with id: {}", discussionId);
+        return ResponseEntity.ok(
+                argumentService
+                        .findAllByDiscussionId(discussionId)
+                        .stream()
+                        .map(argument -> new ArgumentResponseDTO(argument))
+                        .collect(Collectors.toList()));
+    }
+
+    @PostMapping
+    public ResponseEntity<ArgumentResponseDTO> createArgument(@PathVariable Long discussionId,
+                                                              @Valid @RequestBody CreateArgumentDTO createArgumentDTO) {
+        logger.info("Received request to create an argument for discussion with id: {}", discussionId);
+        if (discussionService.findById(discussionId).isPresent()) {
+            Argument argument = argumentService.save(discussionId, createArgumentDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ArgumentResponseDTO(argument));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
