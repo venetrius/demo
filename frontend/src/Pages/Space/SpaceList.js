413458f4c672b69b97fd97dede06673e3d4b1c59
venetrius <gergelygjuhasz@gmail.com>
2023-05-29 01:00:01 +0200

    FE space list can show all spaces or one that is followed by current user

--- frontend/src/Pages/Space/SpaceList.js
+++ frontend/src/Pages/Space/SpaceList.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Row, Col } from 'antd';
+import { Button, Row, Col, Switch } from 'antd';
 import { useNavigate } from "react-router-dom";
 import SpaceItem from './SpaceItem';
 import { useSpaces } from '../../contexts/SpaceContext'
 
 const SpaceList = () => {
-  const { spaces, fetchSpaces } = useSpaces();
+  const { spaces, fetchSpaces, userSpaces, fetchUserSpaces } = useSpaces();
+  const [showUserSpaces, setShowUserSpaces] = useState(false);
   let navigate = useNavigate();
 
   useEffect(() => {
-    setTimeout(function(){
+    setTimeout(() => {
       fetchSpaces();
+      fetchUserSpaces();
     }, 100);
   }, []);
 
@@ -18,13 +20,26 @@ const SpaceList = () => {
     navigate('/new-space');
   };
 
+  const handleShowUserSpacesChange = (checked) => {
+    setShowUserSpaces(checked);
+  };
+
+  const spacesToDisplay = showUserSpaces ? userSpaces : spaces;
+
+  console.log({userSpaces, spaces, spacesToDisplay});
+
   return (
     <div>
       <Button type="primary" onClick={handleCreateSpace}>
         Create Space
       </Button>
+      <Switch
+        checkedChildren="Joined Spaces"
+        unCheckedChildren="All Spaces"
+        onChange={handleShowUserSpacesChange}
+        style={{ marginLeft: 20 }} />
       <Row gutter={[16, 16]} style={{ marginTop: 20 }}>
-        {spaces.map((space) => (
+        {spacesToDisplay.map((space) => (
           <Col key={space.id}>
             <SpaceItem space={space} />
           </Col>
