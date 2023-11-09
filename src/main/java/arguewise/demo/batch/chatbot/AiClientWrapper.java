87b9cc94425aecacb7d838eab83d6e070e472fb4
venetrius <gergelygjuhasz@gmail.com>
2023-11-09 15:10:00 +0100

    BE implement IAiClientWrapper

---
+++ src/main/java/arguewise/demo/batch/chatbot/AiClientWrapper.java
@@ -0,0 +1,40 @@
+package arguewise.demo.batch.chatbot;
+
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.service.OpenAiService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AiClientWrapper implements IAiClientWrapper {
+    private final OpenAiService service;
+
+    public AiClientWrapper(@Value("${openai.api-key}") String apiKey) {
+        try {
+            this.service = new OpenAiService(apiKey);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to initialize OpenAiService", e);
+        }
+    }
+
+    public OpenAiService getService() {
+        return service;
+    }
+
+    public ChatCompletionRequest getChatWithDefaultOptions() {
+        ChatCompletionRequest completionRequest = new ChatCompletionRequest();
+        completionRequest.setModel("gpt-3.5-turbo-1106");
+        completionRequest.setMaxTokens(1000);
+        completionRequest.setTemperature(0.9);
+        return completionRequest;
+    }
+
+    public String getResponse(List<ChatMessage> messageList) {
+        ChatCompletionRequest completionRequest = getChatWithDefaultOptions();
+        completionRequest.setMessages(messageList);
+        return service.createChatCompletion(completionRequest).getChoices().get(0).getMessage().getContent();
+    }
+}
