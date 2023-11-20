ce93741958192788f52b3418b431afc96f0c1d40
venetrius <gergelygjuhasz@gmail.com>
2023-11-20 14:36:49 +0100

    BE add Gson adapter for local date time

--- src/main/java/arguewise/demo/config/gson/GsonConfiguration.java
+++ src/main/java/arguewise/demo/config/gson/GsonConfiguration.java
@@ -1,14 +1,21 @@
 package arguewise.demo.config.gson;
 
+import arguewise.demo.batch.chatbot.LocalDateTimeTypeAdapter;
 import com.google.gson.Gson;
+import org.springframework.boot.autoconfigure.gson.GsonBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Configuration
 public class GsonConfiguration {
 
     @Bean
-    public Gson gson() {
-        return new Gson();
+    public GsonBuilderCustomizer typeAdapterRegistration() {
+        return builder -> {
+            builder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter());
+        };
     }
 }
