876e75c531c6ce358031d849eace3d702d97a8a2
venetrius <gergelygjuhasz@gmail.com>
2023-11-28 18:33:39 +0100

    FE Add InfiniteScroll component

---
+++ frontend/src/components/InfiniteScroll/InfiniteScroll.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from 'react';
+import { Row } from 'antd';
+
+const InfiniteScroll = ({ children, obeservableList, hasMore, api }) => {
+    const { fetch, page } = api;
+    const loaderRef = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            const firstEntry = entries[0];
+            if (firstEntry.isIntersecting) {
+                if (!hasMore) return;
+                fetch(page);
+            }
+        }, { threshold: 1.0 });
+
+        const currentLoaderRef = loaderRef.current;
+        if (currentLoaderRef) {
+            observer.observe(currentLoaderRef);
+        }
+
+        return () => {
+            if (currentLoaderRef) {
+                observer.unobserve(currentLoaderRef);
+            }
+        };
+    }, [...obeservableList, loaderRef]);
+
+    return (
+        <Row gutter={[16, 16]} style={{ marginTop: 20 }}>
+            {children}
+            <div ref={loaderRef} style={{ textAlign: 'center', marginTop: '20px' }}>
+                {hasMore ? 'Loading...' : 'No more data to load'}
+            </div>
+        </Row>
+    )
+}
+
+export default InfiniteScroll;
