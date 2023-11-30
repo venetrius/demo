ad721101d8f72cb81e2f58c63396fbbd742b7b69
venetrius <gergelygjuhasz@gmail.com>
2023-11-30 15:03:14 +0100

    FE implement InfiniteScroll for UserDiscussionList

--- frontend/src/Pages/Discsussion/List/UserDiscussionList.jsx
+++ frontend/src/Pages/Discsussion/List/UserDiscussionList.jsx
@@ -1,26 +1,31 @@
 
 import React, { useEffect } from 'react';
-import { Row, Col } from 'antd';
+import { Col } from 'antd';
 import { useUserDiscussions } from '../../../contexts/UserDiscussionContext'
 import { useAuth } from '../../../contexts/AuthContext';
 import DiscussionItem from '../DiscussionItem';
+import InfiniteScroll from '../../../components/InfiniteScroll/InfiniteScroll';
 
 const UserDiscussionList = () => {
     const { authInitialized } = useAuth();
-    const { subscribedDiscussions, fetchSubscribedDiscussions } = useUserDiscussions();
+    const { subscribedDiscussions, fetchSubscribedDiscussions, hasMore, page } = useUserDiscussions();
 
     useEffect(() => {
         fetchSubscribedDiscussions();
     }, [authInitialized]);
 
     return (
-        <Row style={{ marginTop: 20 }}>
+        <InfiniteScroll 
+            observableList={[subscribedDiscussions]} 
+            hasMore={hasMore} 
+            api={{ fetch: fetchSubscribedDiscussions, page }}
+        >
             {subscribedDiscussions.map((discussion) => (
-                <Col span={22} key={discussion.id}>
+                <Col key={`subscribedDiscussions-${discussion.id}`} span={24}>
                     <DiscussionItem discussion={discussion} />
                 </Col>
             ))}
-        </Row>
+        </InfiniteScroll>
     )
 }
 
--- frontend/src/contexts/UserDiscussionContext.js
+++ frontend/src/contexts/UserDiscussionContext.js
@@ -6,11 +6,20 @@ const UserDiscussionContext = createContext();
 
 const UserDiscussionProvider = ({ children }) => {
   const [subscribedDiscussions, setSubscribedDiscussions] = useState([]);
+  const [ inProgress, setInProgress ] = useState(false);
+  const [page, setPage] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
 
   const { token } = useAuth();
 
   const fetchSubscribedDiscussions = async () => {
-    const response = await fetch(`${getApiUrl()}/api/me/discussions`, {
+    if(!token || !hasMore || inProgress) return;
+    setInProgress(true);
+    // TODO add debounce
+    const prevPage = page;
+    const prevContent = subscribedDiscussions;
+
+    const response = await fetch(`${getApiUrl()}/api/me/discussions?page=${page}&size=5`, {
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`,
@@ -18,16 +27,18 @@ const UserDiscussionProvider = ({ children }) => {
     });
 
     if (response.ok) {
-      const data = await response.json();
-      setSubscribedDiscussions(data.content)
-      return data
+      const { content, totalPages } = await response.json();
+      setSubscribedDiscussions([...prevContent, ...content]);
+      setPage(prevPage + 1);
+      setHasMore(totalPages > page);
+      setInProgress(false);
     } else {
       console.log(`Failed to fetch subscribed descriptions.`);
     }
   };
 
   return (
-    <UserDiscussionContext.Provider value={{ subscribedDiscussions, fetchSubscribedDiscussions }}>
+    <UserDiscussionContext.Provider value={{ page, hasMore, subscribedDiscussions, fetchSubscribedDiscussions }}>
       {children}
     </UserDiscussionContext.Provider>
   );
