50f2fe667c838f0995982f24d72cf8f18f141281
venetrius <gergelygjuhasz@gmail.com>
2024-02-16 10:39:41 +0100

    BE: enable general JSON parsing without class definiton

--- src/main/java/arguewise/demo/service/util/IJsonParser.java
+++ src/main/java/arguewise/demo/service/util/IJsonParser.java
@@ -1,6 +1,10 @@
 package arguewise.demo.service.util;
 
+import com.google.gson.JsonObject;
+
 public interface IJsonParser {
     public <T> T parseJson(String jsonString, Class<T> clazz);
 
+    // TODO refactor, don't reference implementation details (JsonObject)
+    public JsonObject parse(String jsonString);
 }
--- src/main/java/arguewise/demo/service/util/JsonParserImpl.java
+++ src/main/java/arguewise/demo/service/util/JsonParserImpl.java
@@ -1,6 +1,8 @@
 package arguewise.demo.service.util;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,11 +10,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class JsonParser implements IJsonParser {
+public class JsonParserImpl implements IJsonParser {
     @Autowired
     private Gson gson;
 
     public <T> T parseJson(String jsonString, Class<T> clazz) {
         return gson.fromJson(jsonString, clazz);
     }
+
+    @Override
+    public JsonObject parse(String jsonString) {
+        JsonParser parser = new JsonParser();
+        return parser.parse(jsonString).getAsJsonObject();
+    }
+
+
 }
